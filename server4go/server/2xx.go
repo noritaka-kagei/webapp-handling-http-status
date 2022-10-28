db825350f7382499495a5246664d6d3594055986
noritaka-kagei <it0ka4ge8i@gmail.com>
2022-10-28 10:50:20 -0400

    add http status 206~226

--- server4go/server/2xx.go
+++ server4go/server/2xx.go
@@ -3,6 +3,8 @@ package server
 import "net/http"
 
 func Set2XX() {
+	http.HandleFunc("/2XX", twoHundredHandler)
+	http.HandleFunc("/2xx", twoHundredHandler)
 	// 200 ~ 226
 	http.HandleFunc("/200", okHandler)
 	http.HandleFunc("/201", createdHandler)
@@ -10,6 +12,17 @@ func Set2XX() {
 	http.HandleFunc("/203", nonAuthInfoHandler)
 	http.HandleFunc("/204", noContentHandler)
 	http.HandleFunc("/205", resetContentHandler)
+	http.HandleFunc("/206", partialContentHandler)
+	http.HandleFunc("/207", multiStatusHandler)
+	http.HandleFunc("/208", alreadyReportedHandler)
+	http.HandleFunc("/226", imUsedHandler)
+}
+
+func twoHundredHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	content := "You can access following HTTP status (2XX)\n"
+	content += "200, 201, 202, 203, 204, 205, 206, 207, 208, 226\n"
+	w.Write([]byte(content))
 }
 
 func okHandler(w http.ResponseWriter, r *http.Request) {
@@ -36,10 +49,18 @@ func resetContentHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(205)
 }
 
-// TODO: 206
+func partialContentHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(206)
+}
 
-// ToDo: 207
+func multiStatusHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(207)
+}
 
-// ToDo: 208
+func alreadyReportedHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(208)
+}
 
-// ToDo: 226
+func imUsedHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(226)
+}
--- server4go/server/server.go
+++ server4go/server/server.go
@@ -1,12 +1,15 @@
 package server
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 )
+
+var port = 8080
 
 func Run() os.Signal {
 	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
@@ -17,7 +20,7 @@ func Run() os.Signal {
 	logger.Println("Setting web server is completed!!")
 
 	logger.Println("Server is running...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
 		logger.Printf("failed to run web server. (%s)\n", err.Error())
 		logger.Println("Server is shutting down...")
 		return nil
@@ -30,4 +33,7 @@ func Run() os.Signal {
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
+	content := fmt.Sprintf("You can get response with HTTP status XXX when you access to localhost:%d/XXX via HTTP protocol.\n", port)
+	content += fmt.Sprintf("You can show avaiable path(XXX) by /NXX that prefix of hundreds place. (ex. localhost:%d/2XX)\n", port)
+	w.Write([]byte(content))
 }

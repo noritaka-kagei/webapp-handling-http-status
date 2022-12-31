911117a6ef48342c2d114645bcf1774cf62af0b9
keronori <it0ka4ge8i@gmail.com>
2022-12-31 23:27:21 +0900

    implement handler for HTTP status code 300-308

--- server4go/server/3xx.go
+++ server4go/server/3xx.go
@@ -1,3 +1,63 @@
 package server
 
-func Set3XX() {}
+import "net/http"
+
+func Set3XX() {
+	// index of HTTP status 300~308
+	http.HandleFunc("/3XX", threeHundredHandler)
+	http.HandleFunc("/3xx", threeHundredHandler)
+
+	// 300 ~ 308
+	http.HandleFunc("/300", multipleChoicesHandler)
+	http.HandleFunc("/301", movedPermanentlyHandler)
+	http.HandleFunc("/302", foundHandler)
+	http.HandleFunc("/303", seeOtherHandler)
+	http.HandleFunc("/304", notModifiedHandler)
+	http.HandleFunc("/305", useProxyHandler)
+	http.HandleFunc("/306", unusedHandler)
+	http.HandleFunc("/307", temporaryRedirectHandler)
+	http.HandleFunc("/308", permanentRedirectHandler)
+}
+
+func threeHundredHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	content := "You can access following HTTP status (3XX)\n"
+	content += "300, 301, 302, 303, 304, 305, 306, 307, 308\n"
+	w.Write([]byte(content))
+}
+
+func multipleChoicesHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(300)
+}
+
+func movedPermanentlyHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(301)
+}
+
+func foundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(302)
+}
+
+func seeOtherHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(303)
+}
+
+func notModifiedHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(304)
+}
+
+func useProxyHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(305)
+}
+
+func unusedHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(306)
+}
+
+func temporaryRedirectHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(307)
+}
+
+func permanentRedirectHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(308)
+}
